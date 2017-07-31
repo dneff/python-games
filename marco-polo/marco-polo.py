6ce9ab89281ca15aa46f05b597042e5b4aeec5f9
Dan Neff <dneff@adobe.com>
2017-07-30 21:25:52 -0700

    adds verify_input to check type, events, and other fixes

--- marco-polo/marco-polo.py
+++ marco-polo/marco-polo.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
-import sys, time, random
+import sys, time, random, bisect
 
 sys.stdout.flush()
 
-
-event_probabilities = [6,4,4,6,6,6,6,4,4,1,6,8,18,10]
-
 months = ['March','May','July','September','November','January']
+event_probabilities = [6,4,4,6,6,6,6,4,4,1,6,8,18,10]    
+event_odds = [sum(event_probabilities[0:x]) for x in range(0,len(event_probabilities))]
+event_odds.sort()
+
 
 class Traveler(object):
     """Represents user"""
@@ -57,12 +58,12 @@ def hunting_init(t):
     """iniitialize hunting skill"""
     print("HUNTING".center(80))
     print("Before you begin your journey, please rate your skill with the crossbow with the crossbow:")
-    print("    (1) Can hit a charging deer at 300 paces")
+    print("    (1) Can hit a charging bear at 300 paces")
     print("    (2) Can hit a deer at 50 paces")
     print("    (3) Can hit a sleeping squirrel at 5 paces")
     print("    (4) Can hit your foot if lucky")
-    skill = int(raw_input("How do you rate yourself? "))
-    skill = verify_range(1,4,skill)
+    skill = verify_input("How do you rate yourself? ", 'int')
+    skill = verify_range(1, 4, skill)
     t.hunting = skill
 
 def supplies_init(t):
@@ -73,12 +74,12 @@ def supplies_init(t):
     print("easily get all the supplies you need.")
 
     print("Several traders offer you camels at prices between {} and {} each.".format(17, 24))
-    camel_price = int(raw_input("How much do you want to pay for a camel? "))
+    camel_price = verify_input("How much do you want to pay for a camel? ",'int')
     camel_price = verify_range(17, 24, camel_price)
     t.beast_quality = camel_price
 
     print("You will need at least 7 camels but no more than 12.")
-    camel_count = int(raw_input("How many camels do you want? "))
+    camel_count = verify_input("How many camels do you want? ", 'int')
     camel_count = verify_range(7, 12, camel_count)
     t.beasts += camel_count
     t.jewels -= camel_price * camel_count
@@ -87,7 +88,7 @@ def supplies_init(t):
     print("\nOne large sack of food costs 2 jewels. You will need at least")
     print("8 sacks to get to Babylon (Baghdad).")
     print("You can carry a max of {} sacks.".format(t.beast_capacity))
-    food = int(raw_input("How many sacks of food do you want? "))
+    food = verify_input("How many sacks of food do you want? ", 'int')
     food = verify_range(8, t.beast_capacity, food)
     t.food = food
     t.jewels -= food * 2
@@ -96,7 +97,7 @@ def supplies_init(t):
     print("\nA skin of oil costs 2 jewels. You should have at least")
     print("6 full skins for cooking in the desert.")
     print("Your camels can carry {} skins.".format(t.beast_capacity - t.beast_load))
-    oil = int(raw_input("How many skins of oil do you want? "))
+    oil = verify_input("How many skins of oil do you want? ",'int')
     oil = verify_range(5, t.beast_capacity - t.beast_load, oil)
     t.oil = oil
     t.jewels -= oil * 2
@@ -104,7 +105,7 @@ def supplies_init(t):
 
 def traveler_barter(t):
     """barter for supplies"""
-    barter = raw_input("You have {} jewels. Do you want to barter here? [y/n] ".format(t.jewels))
+    barter = verify_input("You have {} jewels. Do you want to barter here? [y/n] ".format(t.jewels), 'str')
 
     if verify_yes_no(barter) == 'n':
         return
@@ -117,7 +118,7 @@ def traveler_barter(t):
     arrow_count   = random.randint(1,6) + 6
 
     print("Camels cost {} jewels here".format(camel_price))
-    p = int(raw_input("How many do you want?"))
+    p = verify_input("How many do you want?", 'int')
     camels = verify_range(0,t.jewels//camel_price, p)
     t.beasts += camels
     t.jewels -= camel_price * camels
@@ -127,7 +128,7 @@ def traveler_barter(t):
     shopping = True
     print("Sacks of food cost {} jewels.".format(food_price))
     while shopping:
-        p = int(raw_input("How many do you want?"))
+        p = verify_input("How many do you want?",'int')
         food = verify_range(0,t.jewels//food_price, p)
         if t.food + food + t.oil > t.beast_capacity * 3:
             print("Your camels can't carry that much.")
@@ -139,7 +140,7 @@ def traveler_barter(t):
     shopping = True
     print("Skins of oil cost {} jewels.".format(oil_price))
     while shopping:
-        p = int(raw_input("How many do you want?"))
+        p = verify_input("How many do you want?", 'int')
         oil = verify_range(0,t.jewels//oil_price, p)
         if t.oil + oil + t.food > t.beast_capacity * 3:
             print("Your camels can't carry that much.")
@@ -149,19 +150,19 @@ def traveler_barter(t):
             shopping = False
 
     print("Clothing costs {} jewels each.".format(clothes_price))
-    p = int(raw_input("How many do you want?"))
+    p = verify_input("How many do you want?", 'int')
     clothes = verify_range(0,t.jewels//clothes_price, p)
     t.clothes += clothes
     t.jewels -= clothes * clothes_price
 
     print("Healing balms are {} jewels.".format(balm_price))
-    p = int(raw_input("How many do you want?"))
+    p = verify_input("How many do you want?", 'int')
     medicine = verify_range(0,t.jewels//balm_price, p)
     t.medicines += medicine
     t.jewels -= medicine * balm_price
 
     print("You can get {} arrows for 1 jewel.".format(arrow_count))
-    p = int(raw_input("How many jewels do you want to spend?"))
+    p = verify_input("How many jewels do you want to spend?", 'int')
     arrows = verify_range(0,t.jewels, p) * arrow_count
     t.jewels -= p
     t.weapons += arrows
@@ -180,7 +181,7 @@ def traveler_eat(t):
         print("\t 1: Reasonably well")
         print("\t 2: Adequately")
         print("\t 3: Poorly")
-        eaten = int(raw_input("?"))
+        eaten = verify_input("?", 'int')
         eaten = verify_range(1,3,eaten)
         eaten = 6 - eaten
         if t.food < eaten:
@@ -189,7 +190,7 @@ def traveler_eat(t):
         food_reserve = t.food - eaten
         if food_reserve < 3:
             print("if you eat this much, you'll only have {} sack of food left.".format(food_reserve))
-            a = raw_input("Are you sure?")
+            a = verify_input("Are you sure?", 'str')
             if verify_yes_no(a) == 'n':
                 continue
         t.food -= eaten
@@ -204,7 +205,7 @@ def traveler_starve(t):
     print("You don't have enough food to go on.")
     if t.jewels < 15:
         if t.camels > 0:
-            a = raw_input("Do you want to eat a camel?")
+            a = verify_input("Do you want to eat a camel?", 'str')
             if verify_yes_no(a) == 'y':
                 t.beasts -= 1
                 corpse = random.randint(3,5)
@@ -216,7 +217,7 @@ def traveler_starve(t):
         print("You should have bought food at the market. Now it will cost you.")
         food_price = random.randint(5,9)
         print("The price is {} per sack".format(food_price))
-        count = int(raw_input("How many sacks do you want? "))
+        count = verify_input("How many sacks do you want? ", 'int')
         count = verify_range(0, t.jewels//food_price, count)
         t.food += count
         t.jewels -= food_price * count
@@ -250,12 +251,12 @@ def traveler_sick(t):
 
 def resource_rebase(t):
     """ensure resources can't be negative"""
-    if t.jewels   < 0: t.jewels = 0
-    if t.food     < 0: t.food = 0
-    if t.oil      < 0: t.oil = 0
-    if t.clothes  < 0: t.clothes = 0
-    if t.medicine < 0: t.medicine = 0
-    if t.weapons  < 0: t.weapons = 0
+    if t.jewels    < 0: t.jewels = 0
+    if t.food      < 0: t.food = 0
+    if t.oil       < 0: t.oil = 0
+    if t.clothes   < 0: t.clothes = 0
+    if t.medicines < 0: t.medicines = 0
+    if t.weapons   < 0: t.weapons = 0
 
 def resource_verify(t):
     """check for being out of jewels/clothes"""
@@ -265,7 +266,7 @@ def resource_verify(t):
     if t.beasts < 3:
         print("You push on with your {} camels.".format(t.beasts))
     else:
-        a = raw_input("Would you like to sell a camel? [y/n] ")
+        a = verify_input("Would you like to sell a camel? [y/n] ", 'str')
         if verify_yes_no(a) == 'y':
             sale = 8 + random.randint(1,9)
             print("You get {} jewels for your best camel.".format(sale))
@@ -335,7 +336,7 @@ def traveler_shoot(t):
     start = time.clock()
     hit = False
     while hit == False:
-        shot = raw_input("Type this word: {} :".format(word))
+        shot = verify_input("Type this word: {} :".format(word), 'str')
         if shot.upper() == word:
             hit = True
         else:
@@ -364,9 +365,40 @@ def traveler_hunt(t):
 
 def events(t):
     """special events"""
-
-    pass
-
+    map_events = ['camel_hurt','camel_sick', 'camel_wander', 'bad_water','lost', 'rains', 'food_rotten', 'food_stolen', 'fire', 'burn', 'storms','infection', 'clothing_tear', 'sick', 'bandits']
+    event = map_events[bisect.bisect(event_odds, random.randint(1,100))]
+    if event == 'camel_hurt':
+        print("A camel injured its leg.")
+    elif event == 'camel_sick':
+        print("One of your camels is very sick and can't carry its full load.")
+    elif event == 'camel_wander':
+        print("Two camels wander off. You finally find them after several days.")
+    elif event == 'bad_water':
+        print("Long stretch with bad water. You spend time looking for clean wells.")
+    elif event == 'lost':
+        print("You get lost trying to find an easier route.")
+    elif event == 'rains':
+        print("Heavy rains completely wash away the route.")
+    elif event == 'food_rotten' and t.desert == False:
+        print("Some of your food rots in humid weather.")
+    elif event == 'food_stolen':
+        print("Marauding animals got into your food supply.")
+    elif event == 'fire':
+        print("A fire flares up and destroys some of your food and clothes.")
+    elif event == 'burn' and t.desert == True:
+        print("You get a nasty burn from an oil fire.")
+    elif event == 'storms':
+        print("High winds, sand storms and ferocious heat slow you down.")
+    elif event == 'infection':
+        print("A gash in your leg looks infected. It hurts like the blazes.")
+    elif event == 'clothing_tear':
+        print("Jagged rocks tear your sandals and clothing. You'll need replacements soon.")
+    elif event == 'sick':
+        print("All of you have horrible stomach cramps and intestinal disorder.")
+    elif event == 'bandits':
+        print("Bloodthirsty bandits are attacking your caravan!")
+    
+    
 def desert(t):
     """in the desert?"""
     t.desert == True
@@ -396,56 +428,54 @@ def desert(t):
     else:
         print("You ran out of oil for cooking.")
         if t.oil > 1:
-            t.oil -= random.randint(0,t.oil)
-    if random.randint(1,100) < 25:
+            t.oil -= random.randint(0, t.oil)
+    if random.randint(1, 100) < 25:
         print("You get horribly sick from eating raw and undercooked food.")
         t.oil = 0
         t.health = 1
         t.distance -= 80
-        t.medicine -= 1
+        t.medicines -= 1
 
     events(t)
     resource_rebase(t)
 
 def verify_yes_no(answer):
-    answers = ['y','n','yes','no']
-    while (answer.lower() not in answers):
-        answer = raw_input("Don't understand answer. Enter y/n please:")
+    answers = ['y', 'n', 'yes', 'no']
+    while answer.lower() not in answers:
+        answer = verify_input("Don't understand answer. Enter y/n please:", 'str')
     return answer[0].lower()
 
 def verify_range(min, max, answer):
     response = ''
-    while (min > answer or max < answer):
+    while min > answer or max < answer:
         if answer < min:
             response = 'few'
         else:
             response = 'many'
-        answer = int(raw_input("That is too {}. Try again:".format(response)))
+        answer = verify_input("That is too {}. Try again:".format(response), 'int')
     return answer
 
 def verify_continue():
     raw_input("\nPress ENTER to continue...")
-    pass
 
-def verify_input(string,type):
-    unknown = raw_input(string)
+def verify_input(question, input_type):
+    unknown = raw_input(question)
     verified = False
-    while(verified != True):
-        t = ''
+    while verified != True:
+        test  =  ''
         try:
-            int(unknown)
-            t = 'int'
+            unknown = int(unknown)
+            test = 'int'
         except ValueError:
-            t = 'str'
+            test = 'str'
 
-        if type == t:
+        if input_type == test:
             verified = True
         else:
-            unknown = input("I don't understand: ")
+            unknown = raw_input("I don't understand: ")
+    return unknown
 
-        return unknown
-
-def end_status():
+def end_status(t):
     pass
 
 def main():
@@ -456,12 +486,12 @@ def main():
 
     hunting_init(game)
     supplies_init(game)
-    while(game.total_distance < 6000):
+    while game.total_distance < 6000:
         # advance travel and print calendar
         game.journey += 1
         date_print(game)
         game.total_distance += game.distance
-        game.distance += 40 + game.beast_quality * 20 + random.randint(0,100)
+        game.distance += 40 + game.beast_quality * 20 + random.randint(0, 100)
         print("You have traveled {} miles.".format(game.total_distance))
 
         # print inventory
